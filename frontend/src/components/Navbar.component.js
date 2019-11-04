e29d30a780e40e67ae0b6ab6957e562ae33ff904
Shivanijain21 <shivani.jain@sjsu.edu>
2019-11-03 16:15:46 -0800

    adding signup to navbar

--- frontend/src/components/Navbar.component.js
+++ frontend/src/components/Navbar.component.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Tara</Link>
+        <Link to="/" className="navbar-brand">
+          Tara
+        </Link>
         <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/signin" className="nav-link">Sign In</Link>
-          </li>
-        </ul>
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/signin" className="nav-link">
+                Sign In
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/signup" className="nav-link">
+                Create an Account
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     );
