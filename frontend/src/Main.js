e0866f76d45e4a96e6b989c53caceab6d0a0a83e
purvimisal <purvivmisal@gmail.com>
2019-11-12 00:04:44 -0800

    create profile component

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -4,11 +4,7 @@ import SignUp from "./components/signUp";
 import SignIn from "./components/SignIn";
 import Landing from "./components/Landing";
 import Album from  "./components/Album";
-<<<<<<< HEAD
 import CreateForm from "./components/CreateForm";
-=======
-import ChayForm from "./components/ChayForm";
->>>>>>> 7fba83d89c82becfb72879bb3c3db07c39f5dd2c
 
 import PhotoLinks from "./components/PhotoLinks";
 class Main extends Component {
@@ -20,12 +16,9 @@ class Main extends Component {
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
         <Route path="/album" component={Album} />
-<<<<<<< HEAD
         <Route path="/createform" component={CreateForm}/>
         <Route path="/photo" component={PhotoLinks}/>
-=======
         <Route path="/chayForm" component={ChayForm} />
->>>>>>> 7fba83d89c82becfb72879bb3c3db07c39f5dd2c
       </div>
     );
   }
