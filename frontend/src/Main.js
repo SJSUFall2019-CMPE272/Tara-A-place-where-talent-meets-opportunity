f61c7c2f8cbd79658c3c798cb3e37688c1b85057
purvimisal <purvivmisal@gmail.com>
2019-11-19 10:32:05 -0800

    Home page for Talent

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import SignUp from "./components/signUp";
 import SignIn from "./components/SignIn";
 import Landing from "./components/Landing";
-import Album from "./components/Album";
+import TalentHome from "./components/Talent/Home";
 import UpdateProfile from "./components/Talent/UpdateProfile";
 import CreateJob from "./components/Recruiter/CreateJob";
 class Main extends Component {
@@ -14,7 +14,7 @@ class Main extends Component {
         <Route path="/" exact component={Landing} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
-        <Route path="/album" component={Album} /> 
+        <Route path="/home" component={TalentHome} /> 
         <Route path="/createform" component={UpdateProfile} />
         <Route path="/createjob" component={CreateJob} />
       </div>
