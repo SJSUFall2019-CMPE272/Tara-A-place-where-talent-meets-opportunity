51ccaa2b3c4dabba93d226939f54bd26e966d184
Akshay Elavia <akshay.elavia@sjsu.edu>
2019-11-24 15:45:31 -0800

    Added /path for talent to fetch all talents

--- backend/src/routes/talent.js
+++ backend/src/routes/talent.js
@@ -10,6 +10,31 @@ const router = express.Router();
 //       .send("talent API entry point");
 //   });
 
+router.get("/", function (req, res) {
+
+    var params = {
+        TableName: "tara-talent-demo",
+
+    };
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            // print all the opportunities
+            console.log("Scan succeeded.");
+            data.Items.forEach(function (opportunity) {
+                console.log(opportunity);
+            });
+            res.send(data.Items);
+        }
+    }
+
+});
 
 // to get particular profile
 router.get("/:id", function (req, res) {
@@ -47,7 +72,7 @@ router.post("/:id", function(req, res) {
     console.log(talentId);
     console.log(req.body.itemValues);
 
-    var params = {
+    /* var params = {
         TableName:table,
         Key:{
             "id": talentId
@@ -59,7 +84,7 @@ router.post("/:id", function(req, res) {
             ":a":["Larry", "Moe", "Curly"]
         },
         ReturnValues:"UPDATED_NEW"
-    };
+    }; */
     
 
   });
