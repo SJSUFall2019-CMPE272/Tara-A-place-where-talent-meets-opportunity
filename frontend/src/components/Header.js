8648df7c3009281b6b9731387a417334b7666e36
Purvi Misal <purvivmisal@gmail.com>
2019-11-27 15:24:15 -0800

    changed landing, footer, header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
+
+import Button from 'react-bootstrap/Button'
 
 class Header extends Component {
   render() {
     return (
-     
+
       <header>
-      <Navbar/>
+        <Navbar />
         <div className="head">
-            <h1>Get cast in your next role today</h1>
-            <div>
-              <p>We have the most jobs, the best tools, and expert advice to help you get cast.</p>
-              <div><a className="contact" href="#">Join Us</a></div>
+          <h1>Get cast in your next role today</h1>
+          <div className="col-sm-10">
+            <h5>We have the most jobs, the best tools, and expert advice to help you get cast.</h5>
+            <div className="col-sm-6"><Button size="lg" variant="info">
+              <Link to="/signup" style={{ color: '#FFF' }}>Join Us</Link>
+              </Button>
             </div>
+          </div>
         </div>
-       
+
       </header>
-      
+
     );
   }
 }
