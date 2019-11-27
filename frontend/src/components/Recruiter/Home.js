abbca61550f20a8c0401e553fb13545f9197bf57
purvimisal <purvivmisal@gmail.com>
2019-11-27 14:30:43 -0800

    changes in Home, added talentdetail component, aded route

--- frontend/src/components/Recruiter/Home.js
+++ frontend/src/components/Recruiter/Home.js
@@ -19,12 +19,51 @@ import CheckIcon from '@material-ui/icons/Check';
 import Fab from '@material-ui/core/Fab';
 import { Modal, Button as RButton } from "react-bootstrap";
 import util from "../../utils";
+import { Link } from "react-router-dom";
 
 
 
-import Link from "react-router-dom";
+
 import axios from "axios";
 import { red } from "@material-ui/core/colors";
+
+const Talent = props => (
+    <Grid item xs={12} sm={6} md={4}>
+        <Card className={useStyles.card}>
+            <CardMedia
+                className={useStyles.cardMedia}
+                image="https://source.unsplash.com/random"
+                title="Image title"
+            />
+            <CardContent className={useStyles.cardContent}>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {props.talent.name.firstName + " " + props.talent.name.lastName}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Toggle />
+                {/* <Button color="primary"> */}
+
+                {/* </Button> */}
+                <Button size="small" color="primary">
+                    <Link to={"/talentdetail/"+props.talent.id}>View Profile</Link>
+                </Button>
+            </CardActions>
+        </Card>
+    </Grid>
+)
+
+const Toggle = () => {
+    const [isToggledOn, setToggle] = React.useState(false)
+    const toggle = () => setToggle(!isToggledOn)
+    return (
+        <Fab variant="extended" onClick={toggle} size="medium" color="primary" aria-label="add" className={useStyles.margin}>
+            {isToggledOn ? 'UNMATCH' : 'MATCH'}
+        </Fab>
+    )
+}
+
+
 
 const useStyles = makeStyles(theme => ({
     margin: {
@@ -59,7 +98,6 @@ const useStyles = makeStyles(theme => ({
         flexGrow: 1,
     },
 }));
-const cards = [1, 2, 3, 4, 5, 6, 7];
 
 class Home extends Component {
     state = {
@@ -87,6 +125,14 @@ class Home extends Component {
     handleClose = () => {
         this.setState({ showDetailsModal: false, talent: {} })
     }
+
+    talentList() {
+        return this.state.talents.map(currentTalent => {
+            return <Talent talent={currentTalent} key={currentTalent.id}/>;
+        })
+    }
+
+
     render() {
         const labelstyles = {
             fontSize: '20px',
@@ -115,13 +161,13 @@ class Home extends Component {
                             <div className={useStyles.heroButtons}>
                                 <Grid container spacing={2} justify="center">
                                     <Grid item>
-                                        <Button variant="contained" color="primary">
-                                            Main call to action
+                                        <Button variant="outlined" color="primary">
+                                            <Link to="/createjob">Create a Job</Link>
                                         </Button>
                                     </Grid>
                                     <Grid item>
                                         <Button variant="outlined" color="primary">
-                                            Show all posted jobs
+                                            <Link to="#">Show all posted jobs</Link>
                                         </Button>
                                     </Grid>
                                 </Grid>
@@ -131,98 +177,10 @@ class Home extends Component {
                     <Container className={useStyles.cardGrid} maxWidth="md">
                         {/* End hero unit */}
                         <Grid container spacing={4}>
-                            {this.state.talents.map(talent => (
-                                <Grid item key={talent.id} xs={12} sm={6} md={4}>
-                                    <Card className={useStyles.card}>
-                                        <CardMedia
-                                            className={useStyles.cardMedia}
-                                            image="https://source.unsplash.com/random"
-                                            title="Image title"
-                                        />
-                                        <CardContent className={useStyles.cardContent}>
-                                            <Typography gutterBottom variant="h5" component="h2">
-                                                {talent.name.firstName + " " + talent.name.lastName}
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Fab variant="extended" size="medium" color="primary" aria-label="add" className={useStyles.margin}>
-                                                MATCH <CheckIcon />
-                                            </Fab>
-                                            {/* <Button color="primary"> */}
-
-                                            {/* </Button> */}
-                                            <Button size="small" color="primary" onClick={() => this.handleViewDetails(talent)}>
-                                                View Details
-                                            </Button>
-                                        </CardActions>
-                                    </Card>
-                                </Grid>
-                            ))}
+                                {this.talentList()}   
                         </Grid>
                     </Container>
                 </main>
-                <Modal show={this.state.showDetailsModal} onHide={this.handleClose}>
-                    <Modal.Header>
-                        <Modal.Title>{this.state.talent.name && <p>
-                            {this.state.talent.name.firstName} {this.state.talent.name.LastName}
-                        </p>}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <div className="col-sm-12">
-                            <label style={labelstyles}>Email</label>
-                            <p>{this.state.talent.email}</p>
-                        </div>
-                        <div className="col-sm-12">
-                            <label style={labelstyles}>Gender</label>
-                            <p>{this.state.talent.gender}</p>
-                        </div>
-                        {this.state.talent.experience &&
-                            <div className="col-sm-12">
-                                <label style={labelstyles}>Experience</label>
-                                {this.state.talent.experience.map(exp =>
-                                    <div>
-                                        <div className="col-sm-12">
-                                            <label style={labelstyles}>project Name</label>
-                                            <p>{exp.projectName}</p>
-                                        </div>
-                                        <div className="col-sm-12">
-                                            <label style={labelstyles}>project Type</label>
-                                            <p>{exp.projectType}</p>
-                                        </div>
-                                        <div className="col-sm-12">
-                                            <label style={labelstyles}>description</label>
-                                            <p>{exp.description}</p>
-                                        </div>
-                                        <div className="col-sm-12">
-                                            <label style={labelstyles}>role</label>
-                                            <p>{exp.role}</p>
-                                        </div>
-                                    </div>)}
-                            </div>
-                        }
-                        {this.state.talent.gender && <div className="col-sm-12">
-                            <label style={labelstyles}>Gender</label>
-                            {this.state.talent.gender}
-                        </div>}
-                        {this.state.talent.location && <div className="col-sm-12">
-                            <label style={labelstyles}>Address</label>
-                            <p>
-                                {this.state.talent.location.street}
-                            </p>
-                            <p>
-                                {this.state.talent.location.city}
-                            </p>
-                            <p>
-                                {this.state.talent.location.state}
-                            </p>
-                        </div>}
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <RButton variant="secondary" onClick={this.handleClose}>
-                            Close
-                        </RButton>
-                    </Modal.Footer>
-                </Modal>
             </React.Fragment>
         );
     }
