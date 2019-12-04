b0716e5f868db0c2159f10b475c25f151c6b3fff
purvimisal <purvivmisal@gmail.com>
2019-12-04 09:28:59 -0800

    redirect to home on log out

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,18 +16,21 @@ export default class Navbar extends Component {
   render() {
     let RedirectVar = null;
     if (this.state.logout) {
-      RedirectVar = <Redirect to="/" />
+      return <Redirect to="/" />
     }
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        {RedirectVar}
-        <Link to="/" className="navbar-brand">
-          Tara
-        </Link>
         <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
+          <ul className="navbar-nav mr-auto">
 
-          {localStorage.getItem("type") == "talent" ?
+            <li className="navbar-item">
+
+              <Link to="/" className="navbar-brand">
+                Tara
+              </Link>
+            </li>
+
+            {localStorage.getItem("type") == "talent" ?
               <li className="navbar-item">
                 <Link to="/home" className="navbar-brand">
                   Home
@@ -38,12 +41,12 @@ export default class Navbar extends Component {
               </Link>
               </li>}
             {/* </ul> */}
-          {/* </div> */}
+            {/* </div> */}
 
-        {/* <div className="collpase navbar-collapse"> */}
-          {/* <ul className="navbar-nav mr-auto"> */}
+            {/* <div className="collpase navbar-collapse"> */}
+            {/* <ul className="navbar-nav mr-auto"> */}
             {localStorage.getItem("id") ? <li className="navbar-item">
-              <Link onClick={this.handleLogout} className="nav-link">
+              <Link to="/" onClick={this.handleLogout} className="nav-link">
                 Log Out
               </Link>
 
